8cb8ea4d0628680254ea12c61f49ac724048d8b0
carmelcarmel <rapafun@gmail.com>
2022-09-04 13:47:58 +0200

    eth added

--- bot.py
+++ bot.py
@@ -1,13 +1,18 @@
 # bot.py
+from http import client
 import json
 import os
+from pydoc import cli
 import random
+import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 from requests import get
-
+import requests
+import asyncio
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
 bot = commands.Bot(command_prefix='!')
 
@@ -17,13 +22,32 @@ async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
 
-@bot.command()
+@bot.event
+async def on_ready():
+    # (name='test'))
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="cene ETH"))
+    guild = discord.utils.find(lambda g: g.name == GUILD, bot.guilds)
+    print(
+        f'{bot.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})')
+    while not bot.is_closed():
+        channel = bot.get_channel(542748371912491049)
+        url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=ethereum&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h"
+        response = requests.get(url)
+        data = response.json()
+        eth_price = data[0]['current_price']
+        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"ETH {eth_price}$"))
+        # await channel.send(data[0]['price_change_percentage_1h_in_currency'])
+        await asyncio.sleep(60)
+
+
+@ bot.command()
 async def pis(ctx):
     autor_full = str(ctx.author)
     await ctx.send(f'{autor_full[0:-5]}, jebac pis')
 
 
-@bot.command()
+@ bot.command()
 async def zloto(ctx):
     url = f'http://api.nbp.pl/api/cenyzlota/'
     resp = get(url)
@@ -32,7 +56,7 @@ async def zloto(ctx):
     await ctx.send(f"zloto kosztuje {exchange_rate}")
 
 
-@bot.command()
+@ bot.command()
 async def eur(ctx):
     url = f'http://api.nbp.pl/api/exchangerates/rates/a/EUR/'
     resp = get(url)
@@ -41,7 +65,7 @@ async def eur(ctx):
     await ctx.send(f"Ostatni kurs euro to {exchange_rate} PLN")
 
 
-@bot.command()
+@ bot.command()
 async def usd(ctx):
     url = f'http://api.nbp.pl/api/exchangerates/rates/a/usd/'
     resp = get(url)
@@ -50,7 +74,7 @@ async def usd(ctx):
     await ctx.send(f"Ostatni kurs dulara to {exchange_rate} PLN")
 
 
-@bot.command()
+@ bot.command()
 async def covid(ctx):
     url = "https://api.apify.com/v2/key-value-stores/3Po6TV7wTht4vIEid/records/LATEST"
     resp = get(url)
@@ -68,7 +92,7 @@ async def covid(ctx):
     await ctx.send(f"Dane z dnia {txtDate}, umarło w sumie {deceased} \nnowych: {dailyInfected}, pozytywnych: {dailyPositiveTests}")
 
 
-@bot.command()
+@ bot.command()
 async def chf(ctx):
     url = f'http://api.nbp.pl/api/exchangerates/rates/a/chf/'
     resp = get(url)
@@ -77,13 +101,32 @@ async def chf(ctx):
     await ctx.send(f"Ostatni kurs franka to {exchange_rate} PLN")
 
 
-@bot.command()
+@ bot.command()
 async def quad(ctx):
     await ctx.send('Q   N\nU   A\nA   B\nD')
 
 
+@bot.event
+async def on_member_update(before, after):
+    if str(after.status) == "online":
+        print(f"{after.name} has gone {after.status}.")
+
+
+@ bot.command()
+async def eth(ctx):
+    url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=ethereum&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h"
+    response = requests.get(url)
+    data = response.json()
+    await ctx.send(f'''
+aktualna cena: {data[0]['current_price']}$
+zmiana w 24h: {data[0]['price_change_percentage_24h']}
+max w 24h: {data[0]['high_24h']}$
+min w 24h: {data[0]['low_24h']}$
+    ''')
+
+
 # dziala tylko na moje ID
-@bot.command()
+@ bot.command()
 async def hello(ctx):
     if ctx.message.author.id == (690628563296059462):
         await ctx.send('co tam? jak mija dzien?')
@@ -130,9 +173,9 @@ async def on_message(message):
         await message.channel.send(random.choice(gifs))
     if 'gramy?' in message.content:
         await message.channel.send("w <:lostarkcentral:989259285361983498>?")
-    if 'quad' in message.content:
+    if 'quad ' in message.content:
         await message.channel.send("<:quadnab:989258196428398622>")
-    if "papiez" in message.content:
+    if "papiez " in message.content:
         gifs = ['https://tenor.com/view/jp2gmd-pope-john-paul-gif-11713430',
                 'https://tenor.com/view/jp2gmd-pope-dance-gif-8448985',
                 'https://tenor.com/view/rare-pope-jp2-moving-gif-14675228',
@@ -146,9 +189,9 @@ async def on_message(message):
         await message.channel.send('dzey dzey\nwacha kley')
     if 'tusk' in message.content:
         await message.channel.send('https://tenor.com/view/tusk-deautschland-gif-22190125')
-    if 'www.reddit.com' in message.content:
+    if 'reddit.com' in message.content:
         await message.channel.send('https://tenor.com/view/monkey-read-old-news-gif-9870921')
-    if 'pis' in message.content:
+    if ' pis ' in message.content:
         await message.channel.send('https://tenor.com/view/jebacpis-gif-19980630')
     if 'asmond' in message.content:
         await message.channel.send('Asmond? Chyba ASSmrond')
@@ -159,5 +202,5 @@ async def on_message(message):
         await message.channel.send('https://tenor.com/view/morawiecki-ryz-pis-explain-they-have-to-pay-for-a-bowl-of-rice-gif-17432276')
     await bot.process_commands(message)
 
-
+on_member_update('offline', 'online')
 bot.run(TOKEN)
